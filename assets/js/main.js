ffbf2d363153351be2bc0e155dd325136506eac7
Edward Agyeman <edw.agyeman@gmail.com>
2020-03-17 14:50:58 +0100

    update

---
+++ assets/js/main.js
@@ -0,0 +1,78 @@
+// ------------------------------------------------lev1_1-------------------------------------------------------------------
+// console.log(Math.PI)
+// const roundedPI = Math.PI.toFixed(2)
+// console.log(roundedPI)
+
+// ------------------------------------------------lev1_2-------------------------------------------------------------------
+// function runden(x) {
+//     return Math.round(x)
+// }
+
+// let zahlen = [9.45,
+//     193.4464,
+//     0.8596433607,
+//     0.65149992,
+//     2.1807301500,
+//     -2.9406290890,
+//     -24.45,
+//     roundedPI
+// ]
+
+// console.log(zahlen.map(runden))
+
+// ------------------------------------------------lev1_3-------------------------------------------------------------------
+// let randomNum = Math.random()
+// let randomNum1_10 = Math.ceil(Math.random()*10)
+// let randomNum1_100 = Math.ceil(Math.random()*100)
+
+// console.log(randomNum) 
+// console.log(randomNum1_10) 
+// console.log(randomNum1_100) 
+
+// ------------------------------------------------lev1_4-------------------------------------------------------------------
+// let lowest = Math.min(393 ,205 ,479 ,147 ,376 ,267 ,385 ,246 ,190)
+// let highest = Math.max(393 ,205 ,479 ,147 ,376 ,267 ,385 ,246 ,190)
+
+// console.log(lowest)
+// console.log(highest)
+
+// ------------------------------------------------lev2_1-------------------------------------------------------------------
+// let zahlen = [9.45,
+// 193.4464,
+// 0.8596433607,	
+// 0.65149992,
+// 2.1807301500,
+// -2.9406290890,
+// -24.45
+// ]
+
+// function runden(x) {
+//     return Math.round(x)
+// }
+
+// console.log(zahlen.map(runden))
+
+// ------------------------------------------------lev2_2-------------------------------------------------------------------
+// let guessNumber = prompt("Guess a randum number between 1 and 10")
+//     let randomNumber = Math.ceil(Math.random() * 10)
+// function game() {
+//     if (randomNumber == guessNumber) {
+//         return "Good Job, you guessed it!"
+//     } else {
+//         return "Sorry, you're a looser! Try Again"
+//     }
+// }
+// console.log(game())
+
+// ------------------------------------------------lev2_3-------------------------------------------------------------------
+// function minimalWerte() {
+//     let minEltern = Math.min(49, 51);
+//     let dasAlterDerGeschwister = Math.min(26, 0.3, 0.3);
+//     let dasAlterDerFussballspieler = Math.min(26, 23, 33, 23, 35);
+//     return minEltern, dasAlterDerGeschwister, dasAlterDerFussballspieler
+//     return dasAlterDerGeschwister
+//     return dasAlterDerFussballspieler
+    
+// }
+
+// console.log(minimalWerte())
